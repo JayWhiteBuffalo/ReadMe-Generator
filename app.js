996522551d2b318171eb042fe1c7b29ec2b08533
White Buffalo <apothocare@gmail.com>
2022-09-14 02:51:39 -0500

    Seperated License prompt to its own constant. Created function solely for License and badge handling

--- app.js
+++ app.js
@@ -2,88 +2,168 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateReadMe = require('./src/ReadMe-template');
 
-const promptUser = () => {
+
+//Array for badges
+const badgeArr = [
+    {
+        name: 'Apache',
+        License: 'Apache',
+        version: 'Apache 2.0 License',
+        badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]',
+        url: 'https://opensource.org/licenses/Apache-2.0'
+    },
+    {
+        name:'Boost',
+        License: 'Boost',
+        version:'Boost Software License 1.0',
+        badge: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]',
+        url: 'https://www.boost.org/LICENSE_1_0.txt'
+    },
+    {
+        name: 'BSD3',
+        version: 'BSD 3-Clause License',
+        badge: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]',
+        url: 'https://opensource.org/licenses/BSD-3-Clause'
+    },
+    {
+        name: 'BSD2',
+        version: 'BSD 2-Clause License',
+        badge: '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]',
+        url: 'https://opensource.org/licenses/BSD-2-Clause'
+    },
+    {
+        name: 'GNU',
+        version:'GNU GPL vs3',
+        badge:'[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]',
+        url: 'https://www.gnu.org/licenses/gpl-3.0'
+    },
+    {
+        name:'MIT',
+        version:'MIT License',
+        badge:'[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]',
+        url:'https://opensource.org/licenses/MIT'
+    },
+    {
+        name: 'ISC',
+        version: 'ISC License',
+        badge: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]',
+        url: 'https://opensource.org/licenses/ISC'
+    },
+    
+]
+
+const [Apache, Boost, BSD3, BSD2, GNU, MIT, ISC] = badgeArr;
+
+//validate user input
+const validateInput = (value) => {
+    if (value != "") {
+        return true;
+    } else {
+        return "Must provide an answer"
+    }
+};
+
+const questions =
   //Array of questions for user to be returned
-    return inquirer.prompt([
+    [
         {
             type: 'input',
             name: 'title',
             message: 'What is the Title of this Project?',
-            validate: nameInput => {
-                if(nameInput){
-                    return true;
-                }else{
-                    console.log('You must enter a name for this ReadMe.');
-                    return false;
-                }
-            }
+            validate: validateInput,
         },
         {
             type: 'input',
             name: 'description',
             message: 'Give a detailed description of your Project',
-            validate: nameInput => {
-                if(nameInput){
-                    return true;
-                }else{
-                    console.log('You must enter a description.');
-                    return false;
-                }
-            }
+            validate: validateInput,
         },
         {
-            type: 'input',
-            message: "Table of Contents.",
-            name: "Table of Contents"
+            type: 'confirm',
+            message: "Would you like to include a Table of Contents?",
+            name: "Table of Contents",
+            validate: validateInput
         },
         {
             typer: "input",
             message: "What does the user need to install to run this app?",
-            name: "Installation"
+            name: "Installation",
+            default : "No extra Installation is required"
         },
         {
             type: "input",
             message: "How is the app used? Please give a brief overview of instructions for this app",
-            name: "Usage"
-        },
-        //may change this to options
-        {
-            type: "input",
-            message: "What liscense(s) are being used?",
-            name: "License"
+            name: "Usage",
+            validate: validateInput
         },
         {
             type: "input",
             message: "Who contributed to this project?",
-            name:"Contributing"
-        },
-        {
-            type:"input",
-            message: "Please give contact info for questions and inquireries",
-            name:"Questions"
+            name:"Contributing",
+            validate: validateInput
         },
         {
             type:"input",
             message: "What is your Github username?",
-            name: 'Github'
+            name: 'Github',
+            validate: validateInput,
         },
         {
             type: 'input',
             message: 'What is your email address?',
-            name: 'Email'
-        }
+            name: 'Email',
+            validate: validateInput
+        },
 
-    ])
-}
+    ];
 
-//Start user prompt
-promptUser().then(data => { const readMeMD = generateReadMe(data);
-fs.writeFile('./readMe.md', readMeMD, err => {
-    if (err) throw new Error(err);
-    console.log('Read me created');
-})});
-// function to write ReadMe file
+    //Seperate function for License and badges
+    const askLicense = 
+        [{
+        type: "list",
+        message: "What liscense will being used?",
+        name: "License",
+        choices: [
+            Apache,
+            Boost,
+            BSD3,
+            BSD2,
+            GNU,
+            MIT,
+            ISC,
+        ]
+    }];
+    
+  //function to loop through array with return licensed input and check each object to see if is a match for data out put
 
+ function licenseHandle (data){
+    let license = badgeArr.filter(x => x.name === data.License)
+    let badge = license.map(x => x.badge);
+     console.log(badge);
+ }
+
+
+
+
+       
+
+
+
+function runIt() {
+    inquirer.prompt(askLicense)
+    .then(data => {licenseHandle(data)});
+
+};
+    // .then(data => {const readMeMD = generateReadMe(data);
+    // fs.writeFile('./readMe.md', readMeMD, err => {
+    //     if (err) throw new Error(err);
+    //     console.log('Read me created');
+    // })});
+
+
+
+
+runIt();
 
 // const printReadMeData = (readMeDataArr) => {
 //     console.log(readMeDataArr);
--- src/ReadMe-template.js
+++ src/ReadMe-template.js
@@ -1,10 +1,23 @@
+
+
+
+//get user license string from inquire prompt
+// take userString to rference corresponding license arr obj thru deconstruc arr
+//usrString.badge to place corrct badge
+
+
+
+
+
+
 module.exports = templateData => {
-    const {...data} = templateData;
+
+    let {...data} = templateData;
     return `
     # ${data.title}
-    https://github.com/${data.Github}/${data.Title}
+    https://github.com/${data.Github}/${data.title}
     # Description
-    ${data.Description}
+    ${data.description}
     # Table of Contents
     * [Installation](#installation)
     * [Usage](#usage)
@@ -15,12 +28,13 @@ module.exports = templateData => {
     The following necessary dependencies must be installed to run the application
     # Usage
     In order to use this app, ${data.Usage}
-    #License
+    # License
     This project is licensed under the ${data.License} license.
-    ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+    
     # Contributing
     Contributors: ${data.Contributing}
     # Questions
     If you have any questions, contact ${data.Email}
     `
 }
+
